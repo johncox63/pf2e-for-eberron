c86551a417826fc485612c8986a48193419e2602
John Cox <john.d.cox@gmail.com>
2023-06-08 07:12:13 -0400

    Updating

---
+++ dev/constants.py
@@ -0,0 +1,26 @@
+# Constants.py
+import os
+ 
+BASE_DIR = os.getcwd()
+
+PACK_NAME = 'pf2e-for-eberron'
+
+COMPENDIUM_ITEM_LIST = [
+     'actions'
+    ,'ancestries'
+    ,'ancestry-features'
+    ,'backgrounds'
+    ,'class-features'
+    ,'classes'
+    ,'deities'
+    ,'equipment'
+    ,'feat-effects'
+    ,'feats'
+    ,'heritages'
+    ,'spell-effect'
+    ,'spells'
+]
+
+COMPENDIUM_JOURNAL_LIST = [
+    'lore'
+]
